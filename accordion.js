5dfe08b2db99a5ceb099576df6b3a992c02266a7
John <mjohncorbet@gmail.com>
2023-12-19 05:00:00 +0800

    set accessibility on faq accordion

--- accordion.js
+++ accordion.js
@@ -1,13 +1,27 @@
 const faqRows = document.querySelectorAll('.faq-row');
 
 function toggleFaq() {
+    const question = this.querySelector('.question h3');
+
     if (this.hasAttribute('data-active')) {
         this.removeAttribute('data-active');
+        question.setAttribute('data-expanded', 'false');
     } else {
         this.setAttribute('data-active', '');
+        question.setAttribute('data-expanded', 'true');
     }
 }
 
 faqRows.forEach(faq => {
     faq.addEventListener('click', toggleFaq);
 });
+
+// accessibility
+const questions = document.querySelectorAll('.faq-row .question h3');
+
+questions.forEach(question => question.addEventListener('keydown', (e) => {
+    if (e.key === ' ' || e.key === 'Enter') {
+        const faqRow = question.closest('.faq-row');
+        faqRow.click();
+    }
+}));
